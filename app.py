7fe49a65717ddb5b830726e06a70cea7947b4646
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-11 21:22:06 +0200

    Added post and get methods

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    if request.method == 'POST':
+        print('Incoming...')
+        print(request.get_json(force=True))
+        return 'OK', 200
+    else:
+        message = {'greeting': 'Hello from Python!'}
+        return jsonify(message)
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def test_page():
+    test = 'test'
+    return render_template('index.html', test=test)
 
 
 if __name__ == '__main__':
