29034f6196fca27117a2da70548212204c717b00
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-12 21:16:27 +0200

    Added Author and Post objects

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, json
 
 app = Flask(__name__)
+
+
+class Author:
+    def __init__(self, name):
+        # self.email = email
+        # self.profile_id = profile_id
+        self.name = name
+        # self.avatar = avatar
+        # self.footer = footer
+
+
+class Post:
+    def __init__(self, id_post, text, author):
+        self.id_post = id_post
+        self.author = author
+        self.text = text
 
 
 @app.route('/hello', methods=['GET', 'POST'])
@@ -10,7 +26,11 @@ def hello_world():
         print(request.get_json(force=True))
         return 'OK', 200
     else:
-        message = {'greeting': 'Hello from Python!'}
+        author = Author('Bryan')
+        author = json.dumps(author.__dict__)
+        post = Post(1, 'XD', author)
+        post = json.dumps(post.__dict__)
+        message = post
         return jsonify(message)
 
 
