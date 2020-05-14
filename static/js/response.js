da008ee73bcdced9952e0c802e99a6701b64032d
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-14 23:53:20 +0200

    Deleted unnecessary code

--- static/js/response.js
+++ static/js/response.js
@@ -1,73 +1,47 @@
-// GET is the default method, so we don't need to set it
-fetch('/hello')
+fetch('/hello', {})
     .then(function (response) {
 
-        return response.text();
-
-});
-// Send the same request
-fetch('/hello', {
-})
-    .then(function (response) {
-
-        // But parse it as JSON this time
         return response.json();
     })
     .then(function (json) {
-
-        // Do anything with it!
-        console.log('GET response as JSON:');
         json = JSON.parse(json);
 
         let idPost = json['id_post'];
         let text = json['text'];
 
         let author = JSON.parse(json['author']);
+
         let authorEmail = author['email'];
-        let authorName = author['name'];
         let authorIdProfile = author['id_profile'];
+        let authorName = author['name'];
         let authorAvatar = author['avatar'];
         let authorFooter = author['footer'];
 
-        let result = document.createTextNode(idPost + " " + text + " " + authorEmail + " " +
-            authorIdProfile + " " + authorAvatar + " " + authorFooter);
-        // authorName = document.createTextNode(' ' + authorName);
-        document.getElementById('post').appendChild(result);
-        let authorNameValue = document.getElementById('authorName').getAttribute('value');
-        document.getElementById('authorName').setAttribute('value', authorNameValue);
+        document.getElementById('authorNameGet').innerText += ' ' + authorName;
+        let authorEmailGet = document.getElementById('authorEmailGet');
+        authorEmailGet.innerText += authorEmail;
+        authorEmailGet.setAttribute('href', 'mailto:' + authorEmail);
+        document.getElementById('authorFooterGet').innerText += authorFooter;
+        let authorAvatarGet = document.getElementById('authorAvatarGet');
+        authorAvatarGet.setAttribute('src', authorAvatar);
 
-
-
-        let xd = document.getElementById('post').textContent;
-        // authorName = document.getElementById('authorName').getAttribute('value');
-        console.log(authorName);
-        let lol = json;
-        lol['text'] = authorName;
-        console.log(lol['text']);
-        xd['text'] = 'xdd';
-        console.log(xd);
-        fetch('/hello', {
-
-            // Specify the method
-            method: 'POST',
-            async: false,
-            // A JSON payload
-            body: JSON.stringify(json)
-        }).then(function (response) {
-            // authorName = document.getElementById('authorName').getAttribute('value');
-            // let authorNameValue = authorName;
-            // document.getElementById('authorName').setAttribute('value', authorNameValue);
-            // // authorName = author['name'];
-            // console.log(authorName);
-            return response.text();
-
-        }).then(function (text) {
-
-            console.log('POST response: ');
-
-            // Should be 'OK' if everything was successful
-            console.log(text);
-        });
-
-        console.log(author['name']);
+        document.getElementById('postTextGet').innerText += text;
     });
+
+fetch('/hello', {
+
+    // Specify the method
+    method: 'POST',
+    async: false,
+    // A JSON payload
+    body: JSON.stringify({"Author": 'o'})
+}).then(function (response) {
+    return response.text();
+
+}).then(function (text) {
+
+    console.log('POST response: ');
+
+    // Should be 'OK' if everything was successful
+    console.log(text);
+});
